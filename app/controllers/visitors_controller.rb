e06cf3cbb31dd282671ed7dac25728b61c0da4e1
James Michiemo <james.michiemo@gmail.com>
2014-09-28 22:21:02 -0500

    add layout design

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,5 +2,6 @@ class VisitorsController < ApplicationController
   def index
     @consumer = OAuth::Consumer.new(ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {:site=>'http://localhost:3000'})
     @accesstoken = OAuth::AccessToken.new(@consumer, session[:token], session[:secret])
+
   end
 end
