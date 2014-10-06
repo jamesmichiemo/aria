21ea4bd8abc666987cd299e34fb33aeeaaa422b1
James Michiemo <james.michiemo@gmail.com>
2014-10-06 18:49:32 -0400

    prepare OAuth site url for production environment

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,13 +2,13 @@ require 'json'
 
 class TracksController < ApplicationController
   def index
-    @consumer = OAuth::Consumer.new(ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {:site=>'http://localhost:3000'})
+    @consumer = OAuth::Consumer.new(ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {:site=>ENV['OAUTH_CONSUMER_SITE']})
     @accesstoken = OAuth::AccessToken.new(@consumer, session[:token], session[:secret])
     @tracks = JSON.parse(@accesstoken.get('https://oauth-api.beatport.com/catalog/3/most-popular').body)
   end
 
   def search
-    @consumer = OAuth::Consumer.new(ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {:site=>'http://localhost:3000'})
+    @consumer = OAuth::Consumer.new(ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {:site=>ENV['OAUTH_CONSUMER_SITE']})
     @accesstoken = OAuth::AccessToken.new(@consumer, session[:token], session[:secret])
     @tracks = JSON.parse(@accesstoken.get("https://oauth-api.beatport.com/catalog/3/search/?query=#{URI.encode(params[:query])}").body)
     render 'index'
