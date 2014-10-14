cd4adb38b6f8df03d624ffff1739aedfc7772790
James Michiemo <james.michiemo@gmail.com>
2014-10-14 13:18:45 -0400

    add circle of fifths algorithm filter

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -18,6 +18,7 @@ class TracksController < ApplicationController
     @consumer = OAuth::Consumer.new(ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {:site=>ENV['OAUTH_CONSUMER_SITE']})
     @accesstoken = OAuth::AccessToken.new(@consumer, session[:token], session[:secret])
     @tracks = JSON.parse(@accesstoken.get("https://oauth-api.beatport.com/catalog/3/tracks/similar?id=#{URI.encode(params[:id])}").body)
+    @track_key = params[:key]
   end
 
   private
@@ -25,3 +26,4 @@ class TracksController < ApplicationController
     params.require(:search).permit(:query)
   end
 end
+
--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -8,5 +8,187 @@ module TracksHelper
   def collect_artists(hash)
     hash.map { |a| a["name"] if a["type"] == "artist"}.compact * ", " unless hash.nil?
   end
+
+  def encode_key(string)
+    if string == "G♯min"
+      monado = []
+      monado << 13
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "Bmaj"
+      monado = []
+      monado << 13
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "D♯min"
+      monado = []
+      monado << 14
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "F♯maj"
+      monado = []
+      monado << 14
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "A♯min"
+      monado = []
+      monado << 15
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "C♯maj"
+      monado = []
+      monado << 15
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "Fmin"
+      monado = []
+      monado << 16
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "G♯maj"
+      monado = []
+      monado << 16
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "Cmin"
+      monado = []
+      monado << 17
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "D♯maj"
+      monado = []
+      monado << 17
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "Gmin"
+      monado = []
+      monado << 18
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "A♯maj"
+      monado = []
+      monado << 18
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "Dmin"
+      monado = []
+      monado << 19
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "Fmaj"
+      monado = []
+      monado << 19
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "Amin"
+      monado = []
+      monado << 20
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "Cmaj"
+      monado = []
+      monado << 20
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "Emin"
+      monado = []
+      monado << 21
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "Gmaj"
+      monado = []
+      monado << 21
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "Bmin"
+      monado = []
+      monado << 22
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "Dmaj"
+      monado = []
+      monado << 22
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "F♯min"
+      monado = []
+      monado << 23
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "Amaj"
+      monado = []
+      monado << 23
+      monado << '∠＝∞'
+      return monado
+    end
+
+    if string == "C♯min"
+      monado = []
+      monado << 24
+      monado << 'ζ=δτ(ωρ)'
+      return monado
+    end
+
+    if string == "Emaj"
+      monado = []
+      monado << 24
+      monado << '∠＝∞'
+      return monado
+    end
+    '' 
+  end
+
+  def affinity?(monad0, tonality0, monad1, tonality1)
+    if (monad0 == monad1) || # identical, including either major or minor
+       ((monad0 == 24 && monad1 == 13) && tonality0 == tonality1) || # 13 and 24 are affinite if the same tone quality
+       ((monad0 == 13 && monad1 == 24) && tonality0 == tonality1) || # 24 and 13 are affinite if the same tone quality
+       (monad0 == (monad1 - 1) && tonality0 == tonality1) || # affinity sequence
+       (monad0 == (monad1 + 1) && tonality0 == tonality1) # affinity sequence
+      true
+    end
+
+  end
 end
 
