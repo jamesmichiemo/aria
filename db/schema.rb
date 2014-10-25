2182aa646573c4416bdeb84c9d9e0722c033cf69
James Michiemo <james.michiemo@gmail.com>
2014-10-25 13:17:31 -0400

    add favorites model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907165346) do
+ActiveRecord::Schema.define(version: 20141025170919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: true do |t|
+    t.integer  "track_id"
+    t.string   "artist"
+    t.string   "title"
+    t.string   "bpm"
+    t.string   "key"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "favorites", ["user_id", "created_at"], name: "index_favorites_on_user_id_and_created_at", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "provider"
