5bfcb88ed6ed3aa15c007995be930e03a4b47e64
James Michiemo <james.michiemo@gmail.com>
2014-10-26 18:37:02 -0400

    add pagination to results
    shift some logic away from view layer
    remove empty helpers
    revert back to original affinity algorithm
    resolves #3

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -5,19 +5,22 @@ class TracksController < ApplicationController
     @consumer = OAuth::Consumer.new(ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {:site=>ENV['OAUTH_CONSUMER_SITE']})
     @accesstoken = OAuth::AccessToken.new(@consumer, session[:token], session[:secret])
     @tracks = JSON.parse(@accesstoken.get('https://oauth-api.beatport.com/catalog/3/most-popular').body)
+    @results = Kaminari.paginate_array(@tracks["results"].to_a).page(params[:page]).per(10)
   end
 
   def search
     @consumer = OAuth::Consumer.new(ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {:site=>ENV['OAUTH_CONSUMER_SITE']})
     @accesstoken = OAuth::AccessToken.new(@consumer, session[:token], session[:secret])
-    @tracks = JSON.parse(@accesstoken.get("https://oauth-api.beatport.com/catalog/3/search/?query=#{URI.encode(params[:query])}").body)
+    @tracks = JSON.parse(@accesstoken.get("https://oauth-api.beatport.com/catalog/3/search/?query=#{URI.encode(params[:query])}&perPage=150").body)
+    @results = Kaminari.paginate_array(@tracks["results"].to_a.delete_if {|key, value| key["type"] !="track" }).page(params[:page]).per(15)
     render 'index'
   end
 
   def show
     @consumer = OAuth::Consumer.new(ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {:site=>ENV['OAUTH_CONSUMER_SITE']})
     @accesstoken = OAuth::AccessToken.new(@consumer, session[:token], session[:secret])
-    @tracks = JSON.parse(@accesstoken.get("https://oauth-api.beatport.com/catalog/3/tracks/similar?id=#{URI.encode(params[:id])}").body)
+    @similar_tracks = JSON.parse(@accesstoken.get("https://oauth-api.beatport.com/catalog/3/tracks/similar?id=#{URI.encode(params[:id])}&perPage=20").body)
+    @results = Kaminari.paginate_array(@similar_tracks["results"].to_a.delete_if {|key, value| key["type"] !="track" }).page(params[:page]).per(15)
     @track_key = params[:key]
   end
 
