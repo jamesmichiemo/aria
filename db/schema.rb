f5fa1ef76d005190feb999e2f07cf16459c3e86b
James Michiemo <james.michiemo@gmail.com>
2014-10-27 21:38:05 -0400

    add username attribute to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025175102) do
+ActiveRecord::Schema.define(version: 20141027222138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20141025175102) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
 end
